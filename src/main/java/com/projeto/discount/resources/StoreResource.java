7c071c35513ed55ee518c2a87f7b572832acaa39
alissonrhuant3 <121673922+alissonrhuant3@users.noreply.github.com>
2023-12-28 20:38:56 -0300

    Implementado método inserirJogos

--- src/main/java/com/projeto/discount/resources/StoreResource.java
+++ src/main/java/com/projeto/discount/resources/StoreResource.java
@@ -3,22 +3,63 @@ package com.projeto.discount.resources;
 import com.projeto.discount.entities.Store;
 import com.projeto.discount.services.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@RestController
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Controller
 public class StoreResource {
+
+    private static String caminhoImagens = "C:\\Users\\1118346673\\Documents\\imagens\\";
+
 
     @Autowired
     private StoreService ss;
 
-    @RequestMapping("/paginainicial")
+    //Retorna pagina inicial index!
+    @GetMapping("/")
     public ModelAndView store(){
         ModelAndView mv = new ModelAndView("index");
 
         Iterable<Store> store = ss.findAll();
         mv.addObject("store",store);
         return mv;
+    }
+
+    //Retorna pagina inserirjogo
+    @GetMapping(value = "/inserirjogo")
+    public String inserirJogos(){
+        return "page/inserirjogo";
+    }
+
+    //Método para adicionar novo jogo com upload de imagem
+    @PostMapping(value = "/inserirjogo")
+    public ResponseEntity<Object> inserirJogos(@Validated Store store, @RequestParam("file")MultipartFile file) {
+        store = ss.save(store);
+
+        try {
+            if(!file.isEmpty()) {
+                byte[] bytes = file.getBytes();
+                java.nio.file.Path caminho = Paths.get(caminhoImagens+ String.valueOf(store.getCodigoGame()) +file.getOriginalFilename());
+                Files.write(caminho, bytes);
+
+                store.setNomeImagem(String.valueOf(store.getCodigoGame())+file.getOriginalFilename());
+                ss.save(store);
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{codigoGame}")
+                .buildAndExpand(store.getCodigoGame()).toUri();
+        return ResponseEntity.created(uri).body("redirect:/");
     }
 }
